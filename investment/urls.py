3c06a91acb30e2fe591f6438b8883fec84026594
oferww <oferwal10@gmail.com>
2024-06-01 18:52:50 +0300

    add_investment

--- investment/urls.py
+++ investment/urls.py
@@ -7,14 +7,17 @@ from .views import *
 urlpatterns = [
     # Investment
     path('investment/history/', views.investments_list_view, name='my_investments_history'),
-    path('investment/add/', views.add_investment_view, name='add_investment'),
+    path('investment/add/', views.add_investment, name='add_investment'),
     # Investment Portfolio
     path('profile/portfolio/', views.profile_portfolio, name='profile_portfolio'),
-    path('investment/', views.investment_main, name='investments_main'),
+    # path('investment/', views.investment_main, name='investments_main'),
     path('check_positive_amount/', views.check_positive_number, name='check_amount'),
 
+    # from chatgpt
     # path('items/', investmentListCreate.as_view(), name='item-list-create'),
     # path('items/<int:pk>/', investmentDetail.as_view(), name='item-detail'),
-    path('investments/', investment_list),
-    path('investments/<int:id_pk>/', investment_detail)
+
+    # from YouTube
+    # path('investments/', investment_list),
+    # path('investments/<int:id_pk>/', investment_detail)
 ]
--- investment/views.py
+++ investment/views.py
@@ -21,6 +21,8 @@ from rest_framework.decorators import api_view
 from rest_framework import status
 
 
+# from chatgpt
+
 # class investmentListCreate(generics.ListCreateAPIView):
 #     queryset = Investment.objects.all()
 #     serializer_class = investmentSerializer
@@ -30,24 +32,24 @@ from rest_framework import status
 #     queryset = Investment.objects.all()
 #     serializer_class = investmentSerializer
 
-
-@api_view(['GET', 'POST'])
-def investment_list(request):
-    if request.method == 'GET':
-        investments = Investment.objects.all()
-        serializer = investmentSerializer(investments, many=True)
-        return JsonResponse(serializer.data, safe=False)
-
-    if request.method == 'POST':
-        serializer = investmentSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data, status=status.HTTP_201_CREATED)
-        else:
-            # Return a response with the validation errors
-            return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
+# from YouTube
+# @api_view(['GET', 'POST'])
+# def investment_list(request):
+#     if request.method == 'GET':
+#         investments = Investment.objects.all()
+#         serializer = investmentSerializer(investments, many=True)
+#         return JsonResponse(serializer.data, safe=False)
+#
+#     if request.method == 'POST':
+#         serializer = investmentSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return JsonResponse(serializer.data, status=status.HTTP_201_CREATED)
+#         else:
+#             # Return a response with the validation errors
+#             return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+#
 @api_view(['GET', 'PUT', 'DELETE'])
 def investment_detail(request, id_pk, format=None):
 
@@ -70,52 +72,36 @@ def investment_detail(request, id_pk, format=None):
     elif request.method == 'DELETE':
         investment.delete()
         return JsonResponse(status=status.HTTP_204_NO_CONTENT, data=request.data)
+
+
 # Investment
 # @login_required
-@require_http_methods(["GET"])
+# @require_http_methods(["GET"])
 @api_view(['GET'])
 def investments_list_view(request):
     is_form_filled: bool = _check_if_preferences_form_is_filled(request)
-    # if is_form_filled:
-    #     investments: QuerySet[Investment] = _investments_list_view(request)
-    #     context: dict = {
-    #         'investments': investments,
-    #         'is_form_filled': is_form_filled,
-    #         'title': 'Investments',
-    #         'is_investments_view': False,
-    #         'form': InvestmentsHistoryForm,
-    #     }
-    # else:
-    #     context = {
-    #         'is_form_filled': is_form_filled,
-    #         'title': 'Investments',
-    #         'form': InvestmentsHistoryForm,
-    #     }
     if is_form_filled:
+        # just an example, need to sort out authentication
         investor_user = InvestorUser.objects.get(id=5)
         investments = list(Investment.objects.filter(investor_user=investor_user, mode=Investment.Mode.USER).values())
         print(investments)
         return JsonResponse(status=status.HTTP_200_OK, data=investments, safe=False)
+#     else or try except??
 
 
 # @login_required
-@require_http_methods(["GET", "POST"])
-def add_investment_view(request):
+@api_view(["POST"])
+def add_investment(request):
     is_form_filled: bool = _check_if_preferences_form_is_filled(request)
     if is_form_filled is False:
         raise Http404
-    investments: QuerySet[Investment] = _investments_list_view(request)
-    context: dict = {
-        'investments': investments,
-        'title': 'Investments',
-        'is_investments_view': True,
-    }
-    if request.method == 'GET':
-        return render(request, 'investment/add_investment.html', context=context)
-    elif request.method == 'POST':
-        amount: int = int(request.POST.get('amount', -1))
+    # investments: QuerySet[Investment] = _investments_list_view(request)
+    if request.method == 'POST':
+        amount: int = int(request.headers.get('amount', -1))
         if amount > 0:
-            investor_user: InvestorUser = get_object_or_404(InvestorUser, user=request.user)
+            # investor_user: InvestorUser = get_object_or_404(InvestorUser, user=request.user)
+            investor_user = InvestorUser.objects.get(id=5)
+
             Investment.objects.create(investor_user=investor_user, amount=amount,
                                       stocks_collection_number=investor_user.stocks_collection_number)
             investor_user.total_investment_amount += amount
@@ -127,27 +113,35 @@ def add_investment_view(request):
             # Save image
             data_management.view_investment_report(str(request.user.id), amount, stocks_weights, stocks_symbols)
 
-            # send report to user in email
-            subject: str = 'Investment Report From RoboAdvisor'
-            message: str = (
-                'Here is your investment report.\n'
-                f'You have invested {amount} dollars.\n\n\n'
-                'Sincerely yours,\n'
-                'RoboAdvisor team'
-            )
-            recipient_list: list[str] = [request.user.email]
-            attachment_path: str = f'{settings.USER_IMAGES}{str(request.user.id)}/Investment Report.png'
-            web_actions.send_email(
-                subject=subject, message=message, recipient_list=recipient_list, attachment_path=attachment_path
-            )
 
-            return redirect('my_investments_history')
+            # do we want to send an email??
+
+            # # send report to user in email
+            # subject: str = 'Investment Report From RoboAdvisor'
+            # message: str = (
+            #     'Here is your investment report.\n'
+            #     f'You have invested {amount} dollars.\n\n\n'
+            #     'Sincerely yours,\n'
+            #     'RoboAdvisor team'
+            # )
+            # recipient_list: list[str] = [request.user.email]
+            # attachment_path: str = f'{settings.USER_IMAGES}{str(request.user.id)}/Investment Report.png'
+            # web_actions.send_email(
+            #     subject=subject, message=message, recipient_list=recipient_list, attachment_path=attachment_path
+            # )
+
+            # return redirect('my_investments_history')
+            investments = list(
+                Investment.objects.filter(investor_user=investor_user, mode=Investment.Mode.USER).values())
+            investments.insert(0, ('new invetsment is of amount : ', amount))
+            return JsonResponse(status=status.HTTP_200_OK, data=investments, safe=False)
         else:
             raise ValueError('Invalid amount value')
     else:
         raise BadRequest
 
 
+# probably not needed, I think its just for the html view
 # @require_http_methods(["GET"])
 # def _investments_list_view(request) -> QuerySet[Investment]:
 #     page = request.GET.get("page", None)
@@ -171,12 +165,16 @@ def add_investment_view(request):
 
 
 # Investment Portfolio
+
+# I don't think this func is needed, is there a logical need for a func fo or app that will get a request that will
+# just go to the homepage?
 # @login_required
-def investment_main(request):
-    context = {
-        'title': 'Investments',
-    }
-    return render(request, 'investment/investments_main.html', context=context)
+# def investment_main(request):
+#     context = {
+#         'title': 'Investments',
+#     }
+#     return render(request, 'investment/investments_main.html', context=context)
+
 
 
 # @login_required
